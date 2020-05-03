1c6d316bf4167328a544d89c1a91d7174b1aa129
SAI PHANI PARSA <saiphani.parsa@gmail.com>
2020-05-03 18:46:17 +0530

    Update missing_number.py

--- missing_number.py
+++ missing_number.py
@@ -1,3 +1,5 @@
+"""Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array."""
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
